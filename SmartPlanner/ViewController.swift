6dece72530bee7f7532eccfe9dfd97fffa1df98d
AzaanMP <azaanmp2@gmail.com>
2026-04-02 14:50:21 -0700

    Initial Commit

---
+++ SmartPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartPlanner
+//
+//  Created by csuftitan on 4/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
